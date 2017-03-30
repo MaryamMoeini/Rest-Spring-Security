0d8a2c38fe2ba9fa135912e4be94e4ce34668f8b
maryam moeini <moeini_maryam2414@yahoo.com>
2017-03-30 22:03:35 +0800

    Entity development

--- PoweHouse/src/main/java/com/powerhouse/entity/MeterData.java
+++ PoweHouse/src/main/java/com/powerhouse/entity/MeterData.java
@@ -1,10 +1,106 @@
 package com.powerhouse.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class MeterData implements Serializable {
 
+	private static final long serialVersionUID = 2L;
+
+	@Id
+	@GeneratedValue
+	private long id;
+	private long meterId;
+	private String profileName;
+	private String month;
+	private double meterReading;
+	@Temporal(TemporalType.DATE)
+	private Date recordDate;
+	
+	
+	public MeterData(long meterId, String profileName, double meterReading, Date recordDate) {
+		super();
+		this.meterId = meterId;
+		this.profileName = profileName;
+		this.meterReading = meterReading;
+		this.recordDate = recordDate;
+	}
+	
+	
+
+	public MeterData(long meterId, String profileName, String month, double meterReading, Date recordDate) {
+		super();
+		this.meterId = meterId;
+		this.profileName = profileName;
+		this.month = month;
+		this.meterReading = meterReading;
+		this.recordDate = recordDate;
+	}
+
+
+
+	public long getMeterId() {
+		return meterId;
+	}
+
+	public void setMeterId(long meterId) {
+		this.meterId = meterId;
+	}
+
+	public String getProfileName() {
+		return profileName;
+	}
+
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+
+	public Date getRecordDate() {
+		return recordDate;
+	}
+
+	public void setRecordDate(Date recordDate) {
+		this.recordDate = recordDate;
+	}
+
+	public String getMonth() {
+		return month;
+	}
+
+	public void setMonth(String month) {
+		this.month = month;
+	}
+
+
+	public double getMeterReading() {
+		return meterReading;
+	}
+
+	public void setMeterReading(double meterReading) {
+		this.meterReading = meterReading;
+	}
+	
+	public MeterData() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "MeterData [id=" + id + ", meterId=" + meterId + ", profileName=" + profileName + ", month=" + month
+				+ ", meterReading=" + meterReading + ", recordDate=" + recordDate + "]";
+	}
+	
+	
+
+	
 }
--- PoweHouse/src/main/java/com/powerhouse/entity/Profile.java
+++ PoweHouse/src/main/java/com/powerhouse/entity/Profile.java
@@ -10,18 +10,69 @@ import javax.persistence.UniqueConstraint;
 
 @Table(
 	    uniqueConstraints=
-	        @UniqueConstraint(columnNames={"profileName", "month"})
+	        @UniqueConstraint(columnNames={"profileName", "month"}),
+	    name = "profile"
 	)
 
 @Entity
 public class Profile implements Serializable {
 	
+	
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue
-	long id;
+	private long id;
 	
-	String profileName;
-	String month;
-	double fraction;
+	private String profileName;
+	private String month;
+	private double fraction;
+	
+	
+	public Profile(String profileName, String month, double fraction) {
+		super();
+		this.profileName = profileName;
+		this.month = month;
+		this.fraction = fraction;
+	}
+
+	
+	public Profile() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public long getId() {
+		return id;
+	}
+	
+	public String getProfileName() {
+		return profileName;
+	}
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
+	public double getFraction() {
+		return fraction;
+	}
+	public void setFraction(double fraction) {
+		this.fraction = fraction;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Profile [id=" + id + ", profileName=" + profileName + ", month=" + month + ", fraction=" + fraction
+				+ "]";
+	}
+	
+	
 	
 }
